dacc9d5fc8eff856ab22ab1c1d02d59ff493e849
Ashraf50 <ashrafesam00@gmail.com>
2024-07-28 16:50:43 +0300

    add logo

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../cubits/notes_cubit/notes_cubit.dart';
-import 'custom_app_bar.dart';
 import 'notes_list_view.dart';
 
 class NotesViewBody extends StatefulWidget {
@@ -12,24 +11,26 @@ class NotesViewBody extends StatefulWidget {
 }
 
 class _NotesViewBodyState extends State<NotesViewBody> {
-
   @override
   void initState() {
-   BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
-        children: const [
+        children: [
           SizedBox(
             height: 50,
           ),
-          CustomAppBar(
-            title: 'Notes',
-            icon: Icons.search,
+          Text(
+            "Notes",
+            style: TextStyle(
+              fontSize: 28,
+            ),
           ),
           Expanded(
             child: NotesListView(),
